44c88a656509eb9ef5ee8dd0d974f963a9dabee8
Thiago Sperandio <thiagosperandio@gmail.com>
2019-12-16 16:57:58 -0300

    Initial files from scaffolds

---
+++ spring-boot-reactjs-mv/backend/src/main/java/com/tusn/learn/springbootreactjs/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.tusn.learn.springbootreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
